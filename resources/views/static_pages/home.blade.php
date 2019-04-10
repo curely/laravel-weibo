2e9d1cfe0693635a0c96ad706ead26bb243506d1
a <a@adeMacBook-Pro.local>
2019-04-11 00:08:05 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layout.default')
 @section('title', 'home')
 @section('content')
-<h1>主页</h1>
+    <div class="jumbotron">
+        <h1>hello world</h1>
+        <p class="lead">你现在看到的是<a href="#">登录</a>页</p>
+        <p>一切将从这里开始</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">注册</a></p>
+    </div>
 @stop
