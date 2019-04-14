9eef4dd123758ac148e3ae9d320c14521e563a88
a <a@adeMacBook-Pro.local>
2019-04-14 23:36:45 +0800

    增加头尾师徒

---
+++ resources/views/layout/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img class="brand-icon" src="" alt="">
+    <a href="https://learnku.com/laravel/courses" target="_blank">每日练习，刻意精进</a>
+    <div class="float-right">
+        <a href="/about">
+            关于
+        </a>
+    </div>
+</footer>
