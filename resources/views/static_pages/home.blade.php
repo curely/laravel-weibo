c4f9d2ec637a639cb40f04ace0afab9aa6541ef4
a <a@adeMacBook-Pro.local>
2019-04-14 23:58:00 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,6 +5,6 @@
         <h1>hello world</h1>
         <p class="lead">你现在看到的是<a href="#">登录</a>页</p>
         <p>一切将从这里开始</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">注册</a></p>
+        <p><a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a></p>
     </div>
 @stop
