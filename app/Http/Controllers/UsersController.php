a47c999dd59f3d39ee5cbb201dd3950b0ac6d9c8
a <a@adeMacBook-Pro.local>
2019-04-24 23:13:36 +0800

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function store(User $user)
+    {
+        $this->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
