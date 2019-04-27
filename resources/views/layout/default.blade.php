e37eed49c627019656daa37c33ef6b258ab3877a
curely <15170057381@163.com>
2019-04-28 00:21:52 +0800

    登录成功后导航逻辑

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -7,9 +7,12 @@
 <body>
 @include('layout._header')
 <div class="container">
-    @include('shared._messages')
-    @yield('content')
-    @include('layout._footer')
+    <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layout._footer')
+    </div>
 </div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
