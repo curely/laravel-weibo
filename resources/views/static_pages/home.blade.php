1014b60fcd23221130eb0c7d51cad958ef5eb28f
curely <15170057381@163.com>
2019-05-03 23:51:14 +0800

    访问授权

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron">
         <h1>hello world</h1>
-        <p class="lead">你现在看到的是<a href="#">登录</a>页</p>
+        <p class="lead">你现在看到的是<a href="{{ route('login') }}">登录</a>页</p>
         <p>一切将从这里开始</p>
         <p><a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a></p>
     </div>
