2e06fa848c2b793f4bdb468c8e46ced88a439733
curely <15170057381@163.com>
2019-05-03 22:17:53 +0800

    用户退出

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,6 +30,8 @@ class SessionsController extends Controller
 
     public function destroy()
     {
-
+        Auth::logout();
+        session()->flash('success', '你已成功退出');
+        return redirect('login');
     }
 }
