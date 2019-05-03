1afadf04917315c05c21d8f4bf3ce526cb322ede
curely <15170057381@163.com>
2019-05-03 22:25:11 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($userdetail)) {
+        if (Auth::attempt($userdetail, $request->has('remember'))) {
             session()->flash('success', '欢迎回来');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
